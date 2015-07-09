3fa94245fbd6edcfcf75e817d1f91b30029bec1c
Jaroslav <9icoh@mail.ru>
2015-07-09 17:36:12 +0300

    Changes in UI.

---
+++ trank/PsychologyVisitSite/PsychologyVisitSite.WebUI/Scripts/app/Home/mainPageController.js
@@ -0,0 +1,26 @@
+﻿app.controller('MainCtrl',
+function ($scope, $http) {
+    $scope.updateLastInformation = function () {
+        $http({ method: "GET", url: "/api/InformationApi/LastInformation" }).
+           success(function (data) {
+               $scope.lastInformation = data;
+           }).
+           error(function (data, status) {
+               $scope.data = data || "Request failed";
+               $scope.status = status;
+           });
+    };
+
+    $scope.updateImgUrls = function() {
+        $http({ method: "GET", url: "/api/InformationApi/GetMainImgUrls" }).
+            success(function(srcList) {
+                $scope.carouselImages = srcList;
+            }).
+            error(function(data, status) {
+                $scope.data = data || "Request failed";
+                $scope.status = status;
+            });
+    };
+});
+
+
---
+++ trank/PsychologyVisitSite/PsychologyVisitSite.WebUI/Scripts/app/Meetings/meetingEventsController.js
@@ -0,0 +1,14 @@
+﻿function MeetingEventsCtrl($scope, $http) {
+
+    $scope.updateMeetingEvents = function () {
+        $http({ method: "GET", url: "/api/MeetingEventsApi/NearestEvent" }).
+           success(function (data) {
+               $scope.meetingEvents = data;
+           }).
+           error(function (data, status) {
+               $scope.data = data || "Request failed";
+               $scope.status = status;
+           });
+    };
+
+}
